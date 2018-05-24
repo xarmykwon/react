bbba1e8e34166889791e0cbd41c07d81c65cfa66
xarmykwon <lion_kwon@naver.com>
2018-05-24 12:43:47 +0900

    hello gogo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyName from './MyName';
 
 class App extends Component {
   render() {
@@ -10,6 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div>
+          <MyName name="리액트" />
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
